7c2efb544d3645e583040d3624c00cc9e17db77c
kyle.kanlin <gazer.kanlin@gmail.com>
2016-01-21 09:02:47 +0800

    add data binding

--- uia.pdf/src/main/java/uia/pdf/gridbag/GridBagDescriptionView.java
+++ uia.pdf/src/main/java/uia/pdf/gridbag/GridBagDescriptionView.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2015 uia.pdf
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,7 @@ package uia.pdf.gridbag;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.pdfbox.pdmodel.PDPage;
 
@@ -35,7 +36,7 @@ public class GridBagDescriptionView extends DescriptionView {
     private int height;
 
     public GridBagDescriptionView(File file, String name) throws PDFException {
-        this(file, name, null);
+        this(file, name, new TreeMap<String, Object>());
     }
 
     public GridBagDescriptionView(File file, String name, Map<String, Object> data) throws PDFException {
@@ -50,6 +51,14 @@ public class GridBagDescriptionView extends DescriptionView {
         catch (Exception ex2) {
             throw new PDFException(ex2.getMessage(), ex2);
         }
+    }
+
+    public Map<String, Object> getData() {
+        return this.data;
+    }
+
+    public void setData(Map<String, Object> data) {
+        this.data = data;
     }
 
     @Override
